c65e211b65cccb75926c8b0776c54352b9467bc2
Matt Sears <matt@mattsears.com>
2010-03-07 09:34:24 -0500

    Updated the anything-config to search the current git project (and cache) for files - this is much faster than the normal directory search.

--- mattsears/modes/anything.el
+++ mattsears/modes/anything.el
@@ -9,34 +9,52 @@
     (volatile)
     (type . file)))
 
-(defvar anything-c-source-cheat
-  '((name . "Cheat Sheets")
-    (init . (lambda ()
-              (unless (anything-candidate-buffer)
-                (with-current-buffer (anything-candidate-buffer 'global)
-                  (call-process-shell-command
-                   "cheat sheets" nil  (current-buffer))
-                  (goto-char (point-min))
-                  (forward-line 1)
-                  (delete-region (point-min) (point))
-                  (indent-region (point) (point-max) -2)))))
-    (candidates-in-buffer)
-    (action . (lambda (entry)
-                (let ((buf (format "*cheat sheet:%s*" entry)))
-                  (unless (get-buffer buf)
-                    (call-process "cheat" nil (get-buffer-create buf) t entry))
-                  (display-buffer buf)
-                  (set-window-start (get-buffer-window buf) 1))))))
+;; See: http://www.emacswiki.org/emacs/AnythingSources#toc62
+
+(defvar anything-c-source-git-project-files-cache nil "(path signature cached-buffer)")
+    (defvar anything-c-source-git-project-files '((name . "Files from Current GIT Project")
+	(init . (lambda ()
+
+	  		(let* ((anything-git-top-dir (file-truename (magit-get-top-dir (if (buffer-file-name)
+									(file-name-directory (buffer-file-name))
+                  default-directory))))
+
+			(default-directory anything-git-top-dir)
+
+
+			 (signature (magit-shell (magit-format-git-command "rev-parse --verify HEAD" nil))))
+
+		    (unless (and anything-c-source-git-project-files-cache
+				 (third anything-c-source-git-project-files-cache)
+				 (equal (first anything-c-source-git-project-files-cache) anything-git-top-dir)
+				 (equal (second anything-c-source-git-project-files-cache) signature))
+		      (if (third anything-c-source-git-project-files-cache)
+			  (kill-buffer (third anything-c-source-git-project-files-cache)))
+		      (setq anything-c-source-git-project-files-cache
+			    (list anything-git-top-dir
+				  signature
+				  (anything-candidate-buffer 'global)))
+		      (with-current-buffer (third anything-c-source-git-project-files-cache)
+			(dolist (filename (mapcar (lambda (file) (concat default-directory file))
+						  (magit-shell-lines (magit-format-git-command "ls-files" nil))))
+			  (insert filename)
+			  (newline))))
+		    (anything-candidate-buffer (third anything-c-source-git-project-files-cache)))))
+
+        (type . file)
+	(candidates-in-buffer)))
 
 (setq anything-sources
-      (list anything-c-source-buffers
-            anything-c-source-project-files
-            anything-c-source-file-name-history
-            anything-c-source-recentf
+      (list anything-c-source-git-project-files
+         	anything-c-source-git-project-files-cache
+            anything-c-source-bookmarks
+            ;; anything-c-source-project-files
+            ;;anything-c-source-file-name-history
+            ;;anything-c-source-recentf
             ))
 
 (setq fit-frame-inhibit-fitting-flag t)
-(setq anything-selection-face 'twilight-highlight)
+(setq anything-selection-face 'lazy-highlight-face)
 (setq anything-header-face 'font-lock-builtin-face)
 
 (provide 'anything)
