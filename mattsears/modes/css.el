91e6fdefb5c9a28ea1fec7ac32717d4c8354ab63
Matt Sears <matt@mattsears.com>
2010-03-07 09:28:09 -0500

    Highlight the hexcolour-keywords with the actual color so we can see immediately the color in use.

--- mattsears/modes/css.el
+++ mattsears/modes/css.el
@@ -1,11 +1,17 @@
 ;;----------------------------------------------------------------------------
 ;; Css mode
 ;;----------------------------------------------------------------------------
+(add-to-list 'load-path "~/.emacs.d/vendor/css-mode.el")
 (require 'css-mode)
-(autoload 'css-mode "css-mode" "Mode for editing CSS file" t)
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
+
 (setq cssm-newline-before-closing-bracket t)
 (setq cssm-indent-function 'cssm-c-style-indenter)
+(setq cssm-indent-level 2)
+(setq cssm-newline-before-closing-bracket t)
+(setq cssm-indent-function #'cssm-c-style-indenter)
+(setq cssm-mirror-mode t)
 
 (defadvice cssm-complete-property
   (after cssm-complete-add-space activate)
@@ -26,10 +32,34 @@
   "Add electric semicolons to css-mode's \"mirror mode.\""
   (define-key cssm-mode-map (read-kbd-macro ";")  'self-insert-command))
 
+;; Highlights HTML/CSS color specifications
+(defvar hexcolour-keywords
+  '(("#[abcdef[:digit:]]\\{3,6\\}"
+     (0 (let ((colour (match-string-no-properties 0)))
+          (if (or (= (length colour) 4)
+                  (= (length colour) 7))
+              (put-text-property
+               (match-beginning 0)
+               (match-end 0)
+               'face (list :background (match-string-no-properties 0)
+                           :foreground (if (>= (apply '+ (x-color-values
+                                                          (match-string-no-properties 0)))
+                                               (* (apply '+ (x-color-values "white")) .6))
+                                           "black" ;; light bg, dark text
+                                         "white" ;; dark bg, light text
+                                         )))))
+        append))))
+
+(defun hexcolour-add-to-font-lock ()
+  (interactive)
+  (font-lock-add-keywords nil hexcolour-keywords t))
+
 (add-hook 'css-mode-hook
-	  '(lambda ()
-	     (setq tab-width 2)
-		 ;;(define-key css-mode-map [return] 'newline-and-indent)
-		 (setq css-electric-brace-behavior t)
-		 (setq css-electric-semi-behavior t)
-	     (setq css-indent-offset 2)))
+          'hexcolour-add-to-font-lock
+          '(lambda ()
+             (setq tab-width 2)
+             (setq css-indent-level 2)
+             (define-key css-mode-map [return] 'newline-and-indent)
+             (setq css-electric-brace-behavior t)
+             (setq css-electric-semi-behavior t)
+             (setq css-indent-offset 2)))
