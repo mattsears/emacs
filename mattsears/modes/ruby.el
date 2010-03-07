f10515896689523ae7a5e9e8b245ad586ff383f4
Matt Sears <matt@mattsears.com>
2010-03-07 09:31:55 -0500

    Simplified the ruby-mode config and removed a bunch of stuff that I don't use on a regular basis.

--- mattsears/modes/ruby.el
+++ mattsears/modes/ruby.el
@@ -1,19 +1,42 @@
 (require 'ruby-mode)
 
-;;----------------------------------------------------------------------------
-;; Ruby - Rspec
-;;----------------------------------------------------------------------------
-(add-to-list 'load-path "~/.emacs.d/vendor/rspec-mode")
-(require 'rspec-mode)
-(add-hook 'rspec-mode-hook
-          '(lambda ()
-             (setq yas/mode-symbol 'rspec-mode)))
+(eval-after-load 'ruby-mode
+  '(progn
+     (require 'ruby-compilation)
+     (add-hook 'ruby-mode-hook 'inf-ruby-keys)
+     (define-key ruby-mode-map (kbd "RET") 'ruby-newline-and-indent)
+     (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
+     (define-key ruby-mode-map (kbd "C-c l") "lambda")))
 
 ;;----------------------------------------------------------------------------
-;; Ruby - Cucumber
+;; Ruby - Testing
 ;;----------------------------------------------------------------------------
+
+;; Clear the compilation buffer between test runs.
+(eval-after-load 'ruby-compilation
+  '(progn
+     (defadvice ruby-do-run-w/compilation (before kill-buffer (name cmdlist))
+       (let ((comp-buffer-name (format "*%s*" name)))
+         (when (get-buffer comp-buffer-name)
+           (with-current-buffer comp-buffer-name
+             (delete-region (point-min) (point-max))))))
+     (ad-activate 'ruby-do-run-w/compilation)))
+
+(add-hook 'ruby-mode-hook 'coding-hook)
+
+;; RSpec
+(vendor 'rspec-mode)
+(add-to-list 'auto-mode-alist '("_spec.rb$" . rspec-mode))
+(require 'rspec-mode)
+
+;; Shoulda
+(vendor 'shoulda-mode)
+(add-to-list 'auto-mode-alist '("_test.rb$" . shoulda-mode))
+(require 'shoulda-mode)
+
+;; Cucumber
+(add-to-list 'load-path "~/.emacs.d/vendor/cucumber")
 (require 'feature-mode)
-;;(add-to-list 'feature-mode '("\.feature$" . feature-mode)) ;; Doesn't work!
 
 ;;----------------------------------------------------------------------------
 ;; Ruby - haml & sass
@@ -21,26 +44,9 @@
 (require 'sass-mode)
 (require 'haml-mode)
 
-;;----------------------------------------------------------------------------
-;; Ruby - Inferior Mode
-;;----------------------------------------------------------------------------
-(require 'inf-ruby)
-(autoload 'run-ruby "inf-ruby"
-  "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby"
-  "Set local key defs for inf-ruby in ruby-mode")
-(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+(setq auto-mode-alist (cons '("\\.haml$" . haml-mode) auto-mode-alist))
 
-;;----------------------------------------------------------------------------
-;; Ruby - Electric mode
-;;----------------------------------------------------------------------------
-(autoload 'ruby-electric-mode "ruby-electric"
-  "Electric brackes/quotes/keywords for Ruby source" t)
-
-;; Only use ruby-electric for adding 'end'
-(setq ruby-electric-expand-delimiters-list nil)
-(add-hook 'ruby-mode-hook
-          (lambda () (ruby-electric-mode t)))
+(vendor 'ruby-hacks)
 
 ;;----------------------------------------------------------------------------
 ;; Ruby related file types
@@ -53,85 +59,39 @@
 (setq auto-mode-alist (cons '("\\.rake$" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.gemspec$" . ruby-mode) auto-mode-alist))
 
-;;----------------------------------------------------------------------------
-;; Ruby - RI mode for ruby docs
-;;----------------------------------------------------------------------------
-
-(add-to-list 'load-path "~/.emacs.d/vendor/ri-emacs")
-(setq ri-ruby-script (expand-file-name "~/.emacs.d/vendor/ri-emacs/ri-emacs.rb"))
-(autoload 'ri "ri-ruby" "Ri mode" t)
-
 (add-hook 'ruby-mode-hook
-          (lambda ()
-            (require 'ruby-style)
-            (require 'rcodetools)
-            (require 'ruby-compilation)
-            (require 'rspec-mode)
-            (ruby-electric-mode t)
-            (add-hook 'local-write-file-hooks
-                      '(lambda()
-                         (save-excursion
-                           (untabify (point-min) (point-max))
-                           (delete-trailing-whitespace))))
-            (set (make-local-variable 'indent-tabs-mode) 'nil)
-            (set (make-local-variable 'tab-width) 2)
-            (define-key ruby-mode-map "\C-m" 'ruby-reindent-then-newline-and-indent)
-            (define-key ruby-mode-map [return] 'ruby-newline-and-indent)
-            (define-key ruby-mode-map (kbd "C-c s") 'rspec-toggle-spec-and-target)
-            (define-key ruby-mode-map (kbd "C-m") 'ruby-reindent-then-newline-and-indent)))
-
-;;----------------------------------------------------------------------------
-;; Ruby - Syntax checking
-;;----------------------------------------------------------------------------
-(eval-after-load 'ruby-mode
-  '(progn
-     (require 'flymake)
-
-     ;; Invoke ruby with '-c' to get syntax checking
-     (defun flymake-ruby-init ()
-       (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                          'flymake-create-temp-inplace))
-              (local-file (file-relative-name
-                           temp-file
-                           (file-name-directory buffer-file-name))))
-         (list "ruby" (list "-c" local-file))))
-
-     (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-     (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-     (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)
-           flymake-err-line-patterns)
-
-     (add-hook 'ruby-mode-hook
-               (lambda ()
-                 (when (and buffer-file-name
-                            (file-writable-p
-                             (file-name-directory buffer-file-name))
-                            (file-writable-p buffer-file-name))
-                   (local-set-key (kbd "C-c d")
-                                  'flymake-display-err-menu-for-current-line)
-                   (flymake-mode t))))))
-
-(defadvice ruby-do-run-w/compilation (before kill-buffer (name cmdlist))
-  (let ((comp-buffer-name (format "*%s*" name)))
-    (when (get-buffer comp-buffer-name)
-      (kill-buffer comp-buffer-name))))
-(ad-activate 'ruby-do-run-w/compilation)
-
-;;----------------------------------------------------------------------------
-;; Ruby - debugging
-;;----------------------------------------------------------------------------
-(setq ruby-dbg-flags "") ;; no warnings when running with compilation
-(autoload 'rdebug "rdebug" "Ruby debugging support." t)
-(global-set-key [f9] 'gud-step)
-(global-set-key [f10] 'gud-next)
-(global-set-key [f11] 'gud-cont)
-(global-set-key "\C-c\C-d" 'rdebug)
+          (function (lambda ()
+                      (flymake-mode)
+					  (add-hook 'local-write-file-hooks
+					    '(lambda()
+					      (save-excursion
+					        (untabify (point-min) (point-max))
+					        (delete-trailing-whitespace))))
+                      )))
 
 ;;----------------------------------------------------------------------------
 ;; Ruby related functions
 ;;----------------------------------------------------------------------------
 
 ;; Code borrowed from Emacs starter kit
+
+(defun pcomplete/rake ()
+  "Completion rules for the `ssh' command."
+  (pcomplete-here (pcmpl-rake-tasks)))
+
+(defun pcmpl-rake-tasks ()
+   "Return a list of all the rake tasks defined in the current
+projects.  I know this is a hack to put all the logic in the
+exec-to-string command, but it works and seems fast"
+   (delq nil (mapcar '(lambda(line)
+			(if (string-match "rake \\([^ ]+\\)" line) (match-string 1 line)))
+		     (split-string (shell-command-to-string "rake -T") "[\n]"))))
+
+(defun rake (task)
+  (interactive (list (completing-read "Rake (default: default): "
+                                      (pcmpl-rake-tasks))))
+  (shell-command-to-string (concat "rake " (if (= 0 (length task)) "default" task))))
+
 (defun rr (&optional arg)
   "Run a Ruby interactive shell session in a buffer."
   (interactive "P")
@@ -145,11 +105,6 @@
                                  ("yarv" . "irb1.9")))))
     (with-current-buffer "*ruby*"
       (rename-buffer (format "*%s*" impl) t))))
-
-(defun rake (task)
-  (interactive (list (completing-read "Rake (default: default): "
-                                      (pcmpl-rake-tasks))))
-  (shell-command-to-string (concat "rake " (if (= 0 (length task)) "default" task))))
 
 (defun ruby-reindent-then-newline-and-indent ()
   "Reindents the current line then creates an indented newline."
@@ -175,17 +130,14 @@
     (beginning-of-line)
     (search-forward "#" (point-at-eol) t)))
 
-;;----------------------------------------------------------------------------
-;; Ruby - Misc.
-;;----------------------------------------------------------------------------
-
-;; Ruby hacks
-(vendor 'ruby-hacks)
-
-;; Enhance font colors
-(font-lock-add-keywords
- 'ruby-mode
- '(("\\<\\(private\\)" 1 font-lock-function-name-face t))
- '(("\\<\\(protected\\)" 1 font-lock-function-name-face t)))
+(defun ruby-module-path (module)
+  (shell-command-to-string
+   (concat
+    "ruby -e "
+    "\"ret='()';$LOAD_PATH.each{|p| "
+    "x=p+'/'+ARGV[0].gsub('.rb', '')+'.rb';"
+    "ret=File.expand_path(x)"
+    "if(File.exist?(x))};printf ret\" "
+    module)))
 
 (provide 'ruby)
