a59bf983029e2cea046d66b1f19a7477ba27572c
Matt Sears <matt@mattsears.com>
2011-01-22 14:27:54 -0500

    Bump aquamacs version id

--- mattsears/custom.el
+++ mattsears/custom.el
@@ -8,9 +8,10 @@
   ;; If there is more than one, they won't work right.
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-autoface-mode nil)
- '(aquamacs-customization-version-id 172 t)
+ '(aquamacs-customization-version-id 190 t)
  '(aquamacs-save-options-on-quit nil)
  '(aquamacs-scratch-file nil)
+ '(aquamacs-version-check-url nil)
  '(default-frame-alist (quote ((tool-bar-lines . 0) (fringe) (right-fringe) (left-fringe . 1) (vertical-scroll-bars) (menu-bar-lines . 0) (background-color . "#0C1021") (background-mode . dark) (border-color . "#dedede") (cursor-color . "#AFAFAF") (foreground-color . "#F8F8F8") (mouse-color . "sienna1"))))
  '(cursor-type (quote (bar . 1)) )
  '(global-hl-line-mode t)
