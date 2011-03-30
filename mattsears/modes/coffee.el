58e470f795f8868d249fcd26b87687b2da77e018
Matt Sears <matt@mattsears.com>
2011-03-30 10:12:33 -0400

    Updates to coffeescript mode

---
+++ mattsears/modes/coffee.el
@@ -0,0 +1,31 @@
+;; Coffeescript mode
+(add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
+(require 'coffee-mode)
+
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+
+  ;; CoffeeScript uses two spaces.
+  (set (make-local-variable 'tab-width) 2)
+
+  ;; If you don't have js2-mode
+  (setq coffee-js-mode 'javascript-mode)
+
+  ;; If you don't want your compiled files to be wrapped
+  (setq coffee-args-compile '("-c" "--no-wrap"))
+
+  ;; *Messages* spam
+  (setq coffee-debug-mode t)
+
+  ;; Emacs key binding
+  (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
+
+  ;; Riding edge.
+  (setq coffee-command "~/dev/coffee"))
+
+(add-hook 'coffee-mode-hook '(lambda () (coffee-custom)))
+
+
