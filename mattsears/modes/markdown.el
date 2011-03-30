b5aead7c3a3347750bf27f051f8dc317fc89a8b8
Matt Sears <matt@mattsears.com>
2011-03-30 10:11:47 -0400

    md and mkd are mardown files too

--- mattsears/modes/markdown.el
+++ mattsears/modes/markdown.el
@@ -4,6 +4,8 @@
 (autoload 'markdown-mode "markdown-mode.el"
    "Major mode for editing Markdown files" t)
 (setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.mkd" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.article" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.comment" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.txt" . markdown-mode) auto-mode-alist))
