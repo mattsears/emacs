c3977b4885d6321e44535870e277c35b580d1eb3
Matt Sears <matt@mattsears.com>
2012-01-14 10:35:52 -0500

    Disable yasnippet duing magit-mode and change the magit's default colors.

--- mattsears/modes/git.el
+++ mattsears/modes/git.el
@@ -2,3 +2,11 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/magit")
 (require 'magit)
 (autoload 'magit-status "magit" nil t)
+
+(add-hook 'magit-mode-hook
+          (lambda ()
+            (setq yas/dont-activate t)
+            (yas/minor-mode -1)
+            (set-face-background 'magit-item-highlight "#0C1021")
+            (set-face-foreground 'magit-diff-add "#79b958")
+            (set-face-foreground 'magit-diff-del "#d95c47")))
