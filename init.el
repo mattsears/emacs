6315ba2ed2f44cc53e7e6a330ed97e6ead613eb6
Matt Sears <matt@mattsears.com>
2013-01-27 11:11:40 -0500

    Giving package.el and Melpa a try. I think I like it.

--- init.el
+++ init.el
@@ -1,5 +1,21 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
+;; Set up package system
+(defvar my-packages
+  '(clojure-mode clojure-test-mode nrepl paredit parenface)
+  "A list of packages to ensure are installed at launch.")
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 ;; Loat Aquamacs customizations
 (setq custom-file "~/.emacs.d/mattsears/custom.el")
 (load custom-file)
