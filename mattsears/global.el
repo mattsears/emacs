feb1c3afcdf5a80df7b9732a2339faeca9b5ed70
Matt Sears <matt@mattsears.com>
2013-01-29 09:35:19 -0500

    Auto refresh buffers - why didn't I know about this!

--- mattsears/global.el
+++ mattsears/global.el
@@ -265,6 +265,14 @@
   "Minor mode for pseudo-structurally editing Lisp code." t)
 ;; (add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
 
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
+
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
+
 ;; Save the open buffers so they reopen when restarting emacs
 (desktop-save-mode 1)
 (setq desktop-path '("~/.emacs.d/"))
