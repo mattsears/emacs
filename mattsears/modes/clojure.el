7345b18f5e65ac698c25bf50db629f3615e2bb13
Matt Sears <matt@mattsears.com>
2013-06-02 10:55:52 -0400

    Remove unused custom functions.

--- mattsears/modes/clojure.el
+++ mattsears/modes/clojure.el
@@ -3,9 +3,3 @@
 ;;----------------------------------------------------------------------------
 
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-
-(defun my-tab-fix ()
-  (local-set-key [tab] 'indent-or-expand))
-
-;; add hooks for modes you want to use the tab completion for:
-(add-hook 'clojure-mode-hook    'my-tab-fix)
